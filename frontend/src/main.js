8375a0b86005a97e8329a23d90ab5c0515b0dcef
hasininjato-vcls <hasininjato@voisinconsulting.com>
2025-03-12 00:14:26 +0300

    add vuetify

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,9 +3,16 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router/index'
 
+// vuetify
+import { createVuetify } from 'vuetify'
+import 'vuetify/styles'
+
+const vuetify = createVuetify();
+
 const pinia = createPinia()
 const app = createApp(App)
 
 app.use(pinia)
 app.use(router)
+app.use(vuetify)
 app.mount('#app')
